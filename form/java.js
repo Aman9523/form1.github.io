63d14aa6e62787387277e4c205a39c1b00afa089
Aman9523 <100127541+Aman9523@users.noreply.github.com>
2023-02-19 13:11:33 +0530

    Add files via upload

---
+++ form/java.js
@@ -0,0 +1,12 @@
+// const mainHeading=document.getElementById('main-heading');
+// console.log(mainHeading)
+// const mainHeading=document.querySelector('#main-heading');
+// const header=document.querySelector(".header");
+// const navItem=document.querySelector(".nav-item");
+// console.log(navItem);
+// console.log(header);
+// console.log(mainHeading);
+// const mainHeading=document.getElementById("main-heading");
+// console.log(mainHeading.innerText);
+// mainHeading.textContent="This is something else";
+// console.log(mainHeading.textContent);
